8de4008cb7b554fdb23e9feba63d66c2b5a57710
Hank Lin <hank.lin@swag.live>
2022-08-04 10:49:47 +0800

    test time

---
+++ confirm.js
@@ -0,0 +1,23 @@
+const { format, startOfMonth, endOfMonth, eachDayOfInterval, subMinutes } = require('date-fns');
+
+const TODAY = new Date();
+const timezoneOffset = TODAY.getTimezoneOffset() !== 0 ? TODAY.getTimezoneOffset() : -480;
+const TODAY_WITH_TIMEZONE = subMinutes(TODAY, timezoneOffset);
+
+const hours = TODAY.getHours();
+const hours2 = TODAY_WITH_TIMEZONE.getHours();
+
+const startDayOfMonth = format(startOfMonth(TODAY_WITH_TIMEZONE), 'yyyy-MM-dd');
+const lastDayOfMonth = format(endOfMonth(TODAY_WITH_TIMEZONE), 'yyyy-MM-dd');
+
+const dakaDay = format(TODAY, 'yyyy-MM-dd');
+
+console.log({
+  TODAY,
+  TODAY_WITH_TIMEZONE,
+  hours,
+  hours2,
+  startDayOfMonth,
+  lastDayOfMonth,
+  dakaDay,
+});
