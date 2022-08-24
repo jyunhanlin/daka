ddccf4bb2e3f6cdd287e6074e5c4ac24fea8c736
Hank Lin <hank.lin@swag.live>
2022-08-24 10:57:12 +0800

    fix IMMEDIATE_DAKA type error

--- src/env.js
+++ src/env.js
@@ -1,7 +1,9 @@
 const DOMAIN = process.env.FEMAS_DOMAIN;
 const USER_NAME = process.env.FEMAS_USERNAME;
 const USER_PASSWORD = process.env.FEMAS_PASSWORD;
-const IMMEDIATE_DAKA = process.env.IMMEDIATE_DAKA || false;
+const IMMEDIATE_DAKA = process.env.IMMEDIATE_DAKA
+  ? process.env.IMMEDIATE_DAKA !== 'false'
+  : false;
 const DELAY_START_MINS = process.env.DELAY_START_MINS || 5;
 const DELAY_END_MINS = process.env.DELAY_END_MINS || 15;
 const MAX_RETRY_COUNT = process.env.MAX_RETRY_COUNT || 3;
