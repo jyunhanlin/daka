26a0064fbc6ccfab08826e0aec210e0013e7182e
Hank Lin <jyunhanlin@gmail.com>
2022-09-04 22:16:19 +0800

    update env for delay

--- src/env.js
+++ src/env.js
@@ -4,9 +4,15 @@ const USER_PASSWORD = process.env.FEMAS_PASSWORD;
 const IMMEDIATE_DAKA = process.env.IMMEDIATE_DAKA
   ? process.env.IMMEDIATE_DAKA !== 'false'
   : false;
-const DELAY_START_MINS = +process.env.DELAY_START_MINS || 5;
-const DELAY_END_MINS = +process.env.DELAY_END_MINS || 15;
-const MAX_RETRY_COUNT = +process.env.MAX_RETRY_COUNT || 3;
+const DELAY_START_MINS = Number.isNaN(Number(process.env.DELAY_START_MINS))
+  ? 5
+  : +process.env.DELAY_START_MINS;
+const DELAY_END_MINS = Number.isNaN(Number(process.env.DELAY_END_MINS))
+  ? 15
+  : +process.env.DELAY_END_MINS;
+const MAX_RETRY_COUNT = Number.isNaN(Number(process.env.MAX_RETRY_COUNT))
+  ? 3
+  : +process.env.MAX_RETRY_COUNT;
 
 module.exports = {
   DOMAIN,
